817e6c1293578c36eb75a97aa33cf8f25dcc44b4
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-08-18 20:36:13 +0330

    code refactor

--- PolygonAreaCsharp/Polygon.cs
+++ PolygonAreaCsharp/Polygon.cs
@@ -8,7 +8,5 @@ public class Polygon(List<Point> points, IAreaCalculator areaCalculator) {
 	private readonly List<Point>     _points        = points         ?? throw new ArgumentNullException(nameof(points));
 	private readonly IAreaCalculator _areaCalculator = areaCalculator ?? throw new ArgumentNullException(nameof(areaCalculator));
 
-	public double CalculateArea() {
-		return _areaCalculator.CalculateArea(_points);
-	}
+	public double CalculateArea() => _areaCalculator.CalculateArea(_points);
 }
